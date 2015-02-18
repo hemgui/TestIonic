0e06966365ac7e1e0dc9c43ca0d0514bae0cd1dd
hemgui <hemgui@gmail.com>
2015-02-18 16:49:39 +0100

    First commit

---
+++ www/js/app.js
@@ -0,0 +1,114 @@
+angular.module('assetList', ['ionic', 'firebase', 'assetList.controllers'])
+
+.run(function($ionicPlatform, $rootScope, $firebaseAuth, $firebase, $window, $ionicLoading) {
+    $ionicPlatform.ready(function() {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        if (window.cordova && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        }
+        if (window.StatusBar) {
+            StatusBar.styleDefault();
+        }
+
+        $rootScope.userEmail = null;
+        $rootScope.baseUrl = 'https://glaring-fire-1961.firebaseio.com/';
+        var authRef = new Firebase($rootScope.baseUrl);
+        $rootScope.auth = $firebaseAuth(authRef);
+
+        $rootScope.show = function(text) {
+            $rootScope.loading = $ionicLoading.show({
+                content: text ? text : 'Loading..',
+                animation: 'fade-in',
+                showBackdrop: true,
+                maxWidth: 200,
+                showDelay: 0
+            });
+        };
+
+        $rootScope.hide = function() {
+            $ionicLoading.hide();
+        };
+
+        $rootScope.notify = function(text) {
+            $rootScope.show(text);
+            $window.setTimeout(function() {
+                $rootScope.hide();
+            }, 1999);
+        };
+
+        $rootScope.logout = function() {
+            $rootScope.auth.$logout();
+            $rootScope.checkSession();
+        };
+
+        $rootScope.checkSession = function() {
+            var auth = new FirebaseSimpleLogin(authRef, function(error, user) {
+                if (error) {
+                    // no action yet.. redirect to default route
+                    $rootScope.userEmail = null;
+                    $window.location.href = '#/auth/signin';
+                } else if (user) {
+                    // user authenticated with Firebase
+                    $rootScope.userEmail = user.email;
+                    $window.location.href = ('#/asset/list');
+                } else {
+                    // user is logged out
+                    $rootScope.userEmail = null;
+                    $window.location.href = '#/auth/signin';
+                }
+            });
+        }
+    });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+        .state('auth', {
+            url: "/auth",
+            abstract: true,
+            templateUrl: "templates/auth.html"
+        })
+        .state('auth.signin', {
+            url: '/signin',
+            views: {
+                'auth-signin': {
+                    templateUrl: 'templates/auth-signin.html',
+                    controller: 'SignInCtrl'
+                }
+            }
+        })
+        .state('auth.signup', {
+            url: '/signup',
+            views: {
+                'auth-signup': {
+                    templateUrl: 'templates/auth-signup.html',
+                    controller: 'SignUpCtrl'
+                }
+            }
+        })
+        .state('asset', {
+            url: "/asset",
+            abstract: true,
+            templateUrl: "templates/asset.html"
+        })
+        .state('asset.list', {
+            url: '/list',
+            views: {
+                'asset-list': {
+                    templateUrl: 'templates/asset-list.html',
+                    controller: 'myListCtrl'
+                }
+            }
+        })
+        .state('asset.completed', {
+            url: '/completed',
+            views: {
+                'asset-completed': {
+                    templateUrl: 'templates/asset-completed.html',
+                    controller: 'completedCtrl'
+                }
+            }
+        })
+    $urlRouterProvider.otherwise('/auth/signin');
+});
